20b058ad537464b7764ea4296add7be02dedabc6
José Pedro Borges <up201503603@fe.up.pt>
2019-12-18 23:22:51 +0000

    added some tweaks, no city duplicates

--- WeatherApplication/WeatherApplication/WeatherApplication/DetailPage.xaml.cs
+++ WeatherApplication/WeatherApplication/WeatherApplication/DetailPage.xaml.cs
@@ -53,7 +53,7 @@ namespace WeatherApplication
 
             // Weather Animations
             // Real Value
-            //drawingView.setConditions(district.getConditions(), district.getDescription(), (float)district.getWindSpeed(), 70);
+            drawingView.setConditions(district.getConditions(), district.getDescription(), (float)district.getWindSpeed(), 70);
             // Weather Tests
             // Clear Sky
             // drawingView.setConditions("Clear", district.getDescription(), (float)district.getWindSpeed(), 0);
@@ -66,7 +66,7 @@ namespace WeatherApplication
             // Light Thunder
             // drawingView.setConditions("Thunderstorm", "light thunderstorm", (float)district.getWindSpeed(), 70);
             // Heavy Thunder
-            drawingView.setConditions("Thunderstorm", "ragged thunderstorm", (float)district.getWindSpeed(), 70);
+            //drawingView.setConditions("Thunderstorm", "ragged thunderstorm", (float)district.getWindSpeed(), 70);
             // Drizzle
             //drawingView.setConditions("Drizzle", district.getDescription(), (float)district.getWindSpeed(), 70);
             // Snow
